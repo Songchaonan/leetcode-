a655a19a42eab59d20ab5ea92188a82ca6379ce1
Songchaonan <38065200+Songchaonan@users.noreply.github.com>
2018-04-07 13:50:15 +0800

    Update sum2.C

--- sum2.C
+++ sum2.C
@@ -1,3 +1,6 @@
+给定一个整数数列，找出其中和为特定值的那两个数。
+
+你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
 int* twoSum(int* nums, int numsSize, int target) {
    int *a= (int*)malloc(2*sizeof(int));
     for(int i = 0;i<numsSize;i++)  
